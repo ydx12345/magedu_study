a229c60cbb004d7833acfa01b9b079b5fffc4823
徐浩铭 <21599180@qq.com>
2020-03-13 14:41:18 +0800

    可以更改间隔符，加了个函数文档

--- P30014_徐浩铭/第二周作业/字典扁平化-递归实现.py
+++ P30014_徐浩铭/第二周作业/字典扁平化-递归实现.py
@@ -4,14 +4,19 @@ dict2 = {'a':{'b':1,'c':{'2':555}}, 'd':{'e':3,'f':{'g':{'e':4}}}, 'm':{'n':1}}
 d = {'a':1}
 
 
-def dict_flat(dic,newdict=None, prefix=''):
+
+def dict_flat(dic, interval='.', *, newdict=None, prefix=''):
+    """
+    第一个参数是待扁平化的字典，第二个参数是分隔符，剩下的参数不用管
+    返回值是已扁平化的新字典，老字典保持不变，可重复使用，而不变质。
+    """
     if newdict is None:
         newdict = {}
     for k, v in dic.items():
         if isinstance(v, dict):      
-            dict_flat(v, newdict, prefix+k+'.')
+            dict_flat(v, newdict=newdict, prefix=prefix+k+interval)
         else:
             newdict[prefix+k] = v
     return newdict
                 
-print(dict_flat(dict1))
+dict_flat(dict1)
